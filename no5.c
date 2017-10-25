4ebf8e4d2b4ef72285c6354a8619723534c1fc3d
mvicky16 <vickyr.muhammad@gmail.com>
2017-10-25 16:26:31 +0700

    edit soal no 5

--- no5.c
+++ no5.c
@@ -6,6 +6,33 @@
 #include<stdlib.h>
 
 pthread_t tid[2];
+void* playandcount(void *arg){
+    unsigned long i=0;
+    int a, b;
+    pthread_t id=pthread_self();
+    if(pthread_equal(id,tid[0])){
+        FILE *l;
+        char y;
+        char v[1024];
+        l = fopen ("Novel.txt", "r+");
+        while((y = fgetc(l)) != EOF){
+                if(strstr(v,"Ifah")) a++; 
+        }
+        fclose (l);
+    }
+    else if(pthread_equal(id,tid[1])){
+        FILE *l;
+        char y;
+        char vb[1024];
+        l = fopen("Novel.txt", "r+");
+        while((y = fgetc(l)) != EOF){
+                if(strstr(vb,"Fina")) b++;
+        }
+        fclose(l);
+    }
+    printf("Ifah : %d\nFina : %d\n", a,b);
+    return NULL;
+}
 
 int main(void){
     int i=0;
